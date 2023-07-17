1c58dc7af387840223bc147d72b01ca32269d0c8
Petr Fusek <petr.fusek97@gmail.com>
2023-07-17 07:13:04 +0200

    Themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import { MantineProvider, Text } from '@mantine/core';
 
 export default function App() {
     return (
-        <MantineProvider withNormalizeCSS withGlobalStyles>
+        <MantineProvider
+            withNormalizeCSS
+            withGlobalStyles
+            theme={{
+                colorScheme: 'dark',
+                globalStyles: (theme) => ({
+                    body: {
+                        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[1],
+                    },
+                }),
+            }}
+        >
             <Text>Welcome to Mantine!</Text>
         </MantineProvider>
     );
